e7f756ce337d5f79628d4c13b8fefc64d667540a
Sinclair <truly.sinclair@gmail.com>
2025-06-30 23:24:13 -0400

    feat: add corruption audio resource and integrate sound playback
    Introduced a `CorruptionAudio` resource for managing corruption sound effects. Added loading of `corruption.ogg` and updated the setup system to spawn an audio player with playback settings.
    
    Signed-off-by: Sinclair <truly.sinclair@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,24 @@ fn main() {
         .run();
 }
 
+#[derive(Resource, Deref)]
+struct CorruptionAudio(Handle<AudioSource>);
+
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
 ) {
     commands.spawn(Camera2d);
+    let corruption_sound = asset_server.load("sounds/corruption.ogg");
+    let sound = corruption_sound.clone();
+    commands.insert_resource(CorruptionAudio(corruption_sound));
     commands.spawn((
         Mesh2d(meshes.add(Rectangle::default())),
         MeshMaterial2d(materials.add(Color::from(SKY_700))),
         Transform::default().with_scale(Vec3::splat(128.)),
     ));
+
+    let t = commands.spawn((AudioPlayer(sound.clone()), PlaybackSettings::DESPAWN));
 }
