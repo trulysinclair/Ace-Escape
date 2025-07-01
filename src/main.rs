f90711904c0636d56dd9f4d0b2e196513845ccda
Sinclair <truly.sinclair@gmail.com>
2025-06-30 23:54:06 -0400

    refactor: rework corruption audio handling and add sound control systems
    Replaced `CorruptionAudio` resource with a `CorruptionSound` component-based approach. Added `update_speed` system for audio playback speed adjustment and `pause` system for toggling playback with the spacebar. Streamlined setup logic for spawning audio player.
    
    Signed-off-by: Sinclair <truly.sinclair@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,17 @@ fn main() {
 
     App::new()
         .add_systems(Startup, setup)
+        .add_systems(Update, (update_speed, pause))
         .add_plugins(DefaultPlugins)
         .insert_resource(ClearColor(background_color))
         .run();
 }
 
-#[derive(Resource, Deref)]
-struct CorruptionAudio(Handle<AudioSource>);
+// #[derive(Resource, Deref)]
+// struct CorruptionAudio(Handle<AudioSource>);
+
+#[derive(Component)]
+struct CorruptionSound;
 
 fn setup(
     mut commands: Commands,
@@ -21,14 +25,39 @@ fn setup(
     asset_server: Res<AssetServer>,
 ) {
     commands.spawn(Camera2d);
-    let corruption_sound = asset_server.load("sounds/corruption.ogg");
-    let sound = corruption_sound.clone();
-    commands.insert_resource(CorruptionAudio(corruption_sound));
     commands.spawn((
         Mesh2d(meshes.add(Rectangle::default())),
         MeshMaterial2d(materials.add(Color::from(SKY_700))),
         Transform::default().with_scale(Vec3::splat(128.)),
     ));
+    commands.spawn((
+        AudioPlayer::new(asset_server.load("sounds/corruption.ogg")),
+        CorruptionSound,
+    ));
 
-    let t = commands.spawn((AudioPlayer(sound.clone()), PlaybackSettings::DESPAWN));
+    // let corruption_sound = asset_server.load("sounds/corruption.ogg");
+    // let sound = corruption_sound.clone();
+    // commands.insert_resource(CorruptionAudio(corruption_sound));
+    // let t = commands.spawn((AudioPlayer(sound.clone()), PlaybackSettings::DESPAWN));
+}
+
+fn update_speed(music_controller: Query<&AudioSink, With<CorruptionSound>>, time: Res<Time>) {
+    let Ok(sink) = music_controller.single() else {
+        return;
+    };
+
+    sink.set_speed((ops::sin(time.elapsed_secs() / 5.0) + 1.0).max(0.1))
+}
+
+fn pause(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    music_controller: Query<&AudioSink, With<CorruptionSound>>,
+) {
+    let Ok(sink) = music_controller.single() else {
+        return;
+    };
+
+    if keyboard_input.just_pressed(KeyCode::Space) {
+        sink.toggle_playback();
+    }
 }
