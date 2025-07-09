03d940308f46e397edb731c5ef8a68c92cd50e89
Sinclair <truly.sinclair@gmail.com>
2025-07-09 03:07:40 -0400

    feat: integrate `bevy_dev_tools` with FPS overlay support
    - Enabled `bevy_dev_tools` feature in `bevy` dependency for enhanced development utilities.
    - Added `FpsOverlayPlugin` with customizable FPS display settings.
    - Updated `Cargo.toml` and `Cargo.lock` to include `bevy_dev_tools`.
    
    Signed-off-by: Sinclair <truly.sinclair@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,15 @@ use crate::game::GamePlugin;
 use crate::ui::game_menu::GameMenuPlugin;
 use ace_escape::GameState;
 use bevy::audio::{AudioPlugin, SpatialScale};
+use bevy::dev_tools::fps_overlay::{FpsOverlayConfig, FpsOverlayPlugin};
 use bevy::prelude::*;
+use bevy::text::FontSmoothing;
 use ui::splash_screen::SplashScreenPlugin;
 
 fn main() {
     App::new()
         .add_systems(Startup, setup)
-        .add_plugins(
+        .add_plugins((
             DefaultPlugins
                 .set(AudioPlugin {
                     default_spatial_scale: SpatialScale::new_2d(1. / 100.0),
@@ -26,7 +28,25 @@ fn main() {
                     }),
                     ..default()
                 }),
-        )
+            FpsOverlayPlugin {
+                config: FpsOverlayConfig {
+                    text_config: TextFont {
+                        // Here we define size of our overlay
+                        font_size: 42.0,
+                        // If we want, we can use a custom font
+                        font: default(),
+                        // We could also disable font smoothing,
+                        font_smoothing: FontSmoothing::default(),
+                        ..default()
+                    },
+                    // We can also change color of the overlay
+                    text_color: Color::srgb(0.0, 1.0, 0.0),
+                    // We can also set the refresh interval for the FPS counter
+                    refresh_interval: core::time::Duration::from_millis(100),
+                    enabled: true,
+                },
+            },
+        ))
         .add_plugins((SplashScreenPlugin, GameMenuPlugin, GamePlugin))
         .init_state::<GameState>()
         .run();
